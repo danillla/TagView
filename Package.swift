d0a48625482eb22673eb5f2a9e0dcb6bf41b8a53
Daniil Alferov <daniil.alferov@gmail.com>
2022-01-22 22:31:02 +0300

    Initial commit

--- Package.swift
+++ Package.swift
@@ -5,24 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "TagView",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "TagView",
-            targets: ["TagView"]),
+    platforms: [
+        .iOS(.v11)
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(name: "TagView", targets: ["TagView"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "TagView",
-            dependencies: []),
-        .testTarget(
-            name: "TagViewTests",
-            dependencies: ["TagView"]),
+        .target(name: "TagView", path: "Sources/TagView")
     ]
 )
--- Sources/TagView/TagView.swift
+++ Sources/TagView/TagView.swift
@@ -1,6 +1,114 @@
-public struct TagView {
-    public private(set) var text = "Hello, World!"
+//
+//  TagView.swift
+//  TagView
+//
+//  Created by Daniil Alferov on 22.01.2022.
+//
 
-    public init() {
+import UIKit
+
+open class TagView: UIView {
+    
+    private var label: UILabel = UILabel(frame: .zero)
+    private var leadingConstraint: NSLayoutConstraint!
+    private var topConstraint: NSLayoutConstraint!
+    private var trailingConstraint: NSLayoutConstraint!
+    private var bottomConstraint: NSLayoutConstraint!
+    private var widthConstraint: NSLayoutConstraint!
+
+    var insets: UIEdgeInsets = UIEdgeInsets.zero {
+        didSet {
+            self.leadingConstraint.constant = insets.left
+            self.topConstraint.constant = insets.top
+            self.trailingConstraint.constant = insets.right
+            self.bottomConstraint.constant = insets.bottom
+            setNeedsUpdateConstraints()
+        }
     }
+
+    var text: String = "" {
+        didSet {
+            self.label.text = text
+            self.updateSize()
+        }
+    }
+    
+    var textColor: UIColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) {
+        didSet {
+            self.label.textColor = self.textColor
+        }
+    }
+    var font: UIFont = UIFont.systemFont(ofSize: 15.0, weight: .medium) {
+        didSet {
+            self.label.font = self.font
+            self.updateSize()
+        }
+    }
+    
+    var tagColor: UIColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0) {
+        didSet {
+            self.backgroundColor = self.tagColor
+        }
+    }
+    
+    var cornerRadius: CGFloat = 6.0 {
+        didSet {
+            self.layer.cornerRadius = self.cornerRadius
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupDefaultConstaints()
+        setup()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupDefaultConstaints()
+        setup()
+    }
+    
+    private func updateSize() {
+        self.widthConstraint.constant = text.width(withConstrainedHeight: self.label.frame.height, font: self.font)
+        setNeedsUpdateConstraints()
+    }
+    
+    private func setupDefaultConstaints() {
+        self.topConstraint = self.label.topAnchor.constraint(equalTo: self.topAnchor, constant: self.insets.top)
+        self.bottomConstraint = self.bottomAnchor.constraint(equalTo: self.label.bottomAnchor, constant: self.insets.bottom)
+        self.leadingConstraint = self.label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: self.insets.left)
+        self.trailingConstraint = self.trailingAnchor.constraint(equalTo: self.label.trailingAnchor, constant: self.insets.right)
+        self.widthConstraint = self.label.widthAnchor.constraint(equalToConstant: 0.0)
+    }
+    
+    func setup() {
+        self.layer.cornerRadius = self.cornerRadius
+        self.backgroundColor = self.tagColor
+        
+        self.label.textColor = self.textColor
+        self.label.font = self.font
+        self.label.backgroundColor = .clear
+        self.label.lineBreakMode = .byTruncatingMiddle
+        self.label.numberOfLines = 1
+        self.label.textAlignment = .center
+        self.label.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(self.label)
+        self.setupLayout()
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            self.topConstraint,
+            self.trailingConstraint,
+            self.bottomConstraint,
+            self.leadingConstraint,
+            self.widthConstraint,
+         ])
+    }
+    
+    open override class var requiresConstraintBasedLayout: Bool {
+        return true
+    }
+    
 }
